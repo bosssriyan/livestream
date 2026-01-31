205a6c0bc676ccd318f806228c28070dc2c3279d
bosssriyan <bosssriyan05@gmail.com>
2026-01-31 23:00:50 +0600

    Add files via upload

---
+++ channels.js
@@ -0,0 +1,44 @@
+﻿(async () => {
+  const PLAYLISTS = [
+    "https://raw.githubusercontent.com/munim-sah75/Cofs_TV/refs/heads/main/fancode.m3u",
+    "https://iptv-org.github.io/iptv/categories/sports.m3u",
+    "https://raw.githubusercontent.com/siksa40/xPola-Player/main/ARENA.m3u",
+	"https://raw.githubusercontent.com/raid35/channel-links/main/8K.m3u",
+  ];
+
+  function parseM3U(text) {
+    const lines = text.split(/\r?\n/);
+    const out = [];
+    let current = null;
+
+    for (const raw of lines) {
+      const line = raw.trim();
+      if (!line) continue;
+
+      if (line.startsWith("#EXTINF")) {
+        const name = (line.split(",").slice(1).join(",") || "Channel").trim();
+        const logo = (line.match(/tvg-logo="([^"]*)"/i) || [])[1] || "";
+        current = {
+          name,
+          logo,
+          url: "",
+          icon: (name[0] || "T").toUpperCase(),
+          group: "All"
+        };
+      } else if (!line.startsWith("#") && current) {
+        current.url = line;
+        out.push(current);
+        current = null;
+      }
+    }
+    return out;
+  }
+
+  const all = [];
+  for (const u of PLAYLISTS) {
+    const t = await fetch(u).then(r => r.text());
+    all.push(...parseM3U(t));
+  }
+
+  window.channels = all;
+})();
